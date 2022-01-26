bb87cfabb58d0380dd3f489161d0f648414bdc2a
anvisaha <77793977+anvisaha@users.noreply.github.com>
2022-01-26 19:17:36 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,34 @@
+img = "";
+var status = "";
+
+function preload() {
+    img = loadImage('decorations.jpg');
+}
+
+function setup() {
+    canvas = createCanvas(640, 220);
+    canvas.center();
+    
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "Status: Detecting Objects";
+
+}
+
+function modelLoaded() {
+    console.log("model loaded");
+    status = true;
+    objectDetector.detect(img, gotResult);
+}
+
+function gotResult(error, results) {
+    if(error) {
+        console.log(error);
+    }
+    console.log(results);
+    object = results;
+}
+
+function draw() {
+    image(img, 0, 0, 640, 220);
+    
+}
